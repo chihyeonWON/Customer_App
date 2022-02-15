83e256ff683950391d063d147714927af9a74df8
ylose <ylose@DESKTOP-PC9EI8M>
2022-02-15 15:20:12 +0900

    Import ImagePanel & 기본위치설정해제

--- src/Customer_App.java
+++ src/Customer_App.java
@@ -1,11 +1,12 @@
+import java.awt.Dimension;
 import java.awt.EventQueue;
+import java.awt.Graphics;
+import java.awt.Image;
 
 import javax.swing.JFrame;
-
+import javax.swing.JPanel;
 public class Customer_App {
-
 	private JFrame frame;
-
 	/**
 	 * Launch the application.
 	 */
@@ -21,20 +22,19 @@ public class Customer_App {
 			}
 		});
 	}
-
 	/**
 	 * Create the application.
 	 */
 	public Customer_App() {
 		initialize();
 	}
-
 	/**
 	 * Initialize the contents of the frame.
 	 */
 	private void initialize() {
 		frame = new JFrame();
 		frame.setBounds(100, 100, 450, 300);
+		frame.setLocationRelativeTo(null); // 기본위치설정해제
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}
 
@@ -42,15 +42,15 @@ public class Customer_App {
 
 class ImagePanel extends JPanel{
 	 private Image img;
-	 
+
 	 public ImagePanel(Image img) {
 		 this.img = img;
 		 setSize(new Dimension(img.getWidth(null),img.getHeight(null)));
 		 setPreferredSize(new Dimension(img.getWidth(null),img.getHeight(null)));
 		 setLayout(null);
 	 }
-	 
+
 	 public void paintComponent(Graphics g) {
 		 g.drawImage(img, 0, 0, null);
 	 }
-}
+} 
