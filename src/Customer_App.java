00b9371fdf069d7664f9f30223b56cfdd11e9333
ylose <ylose@DESKTOP-PC9EI8M>
2022-02-15 16:13:23 +0900

    Window Builder를 사용한 로그인 페이지 구현

--- src/Customer_App.java
+++ src/Customer_App.java
@@ -6,8 +6,16 @@ import java.awt.Image;
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.JLabel;
+import java.awt.Font;
+import javax.swing.JTextField;
+import javax.swing.SwingConstants;
+import javax.swing.JPasswordField;
+import javax.swing.JButton;
 public class Customer_App {
 	private JFrame frame;
+	private JTextField txtEnterId;
+	private JPasswordField passwordField;
 	/**
 	 * Launch the application.
 	 */
@@ -36,12 +44,42 @@ public class Customer_App {
 		frame = new JFrame();
 		ImagePanel welcomePanel = new ImagePanel(new ImageIcon("C://Users//ylose//eclipse-workspace//Swing Lessons//Image//welcome.jpg").getImage());
 		frame.setSize(welcomePanel.getWidth(), welcomePanel.getHeight());
-		frame.add(welcomePanel);
+		frame.getContentPane().add(welcomePanel);
+		
+		JLabel lblNewLabel = new JLabel("Log In");
+		lblNewLabel.setFont(new Font("Arial", Font.BOLD, 30));
+		lblNewLabel.setBounds(292, 229, 213, 65);
+		welcomePanel.add(lblNewLabel);
+		
+		txtEnterId = new JTextField();
+		txtEnterId.setFont(new Font("Arial", Font.PLAIN, 30));
+		txtEnterId.setToolTipText("Enter ID");
+		txtEnterId.setBounds(374, 315, 281, 42);
+		welcomePanel.add(txtEnterId);
+		txtEnterId.setColumns(10);
+		
+		passwordField = new JPasswordField();
+		passwordField.setBounds(374, 393, 281, 42);
+		welcomePanel.add(passwordField);
+		
+		JLabel lblNewLabel_1 = new JLabel("ID  : ");
+		lblNewLabel_1.setFont(new Font("Arial", Font.BOLD, 33));
+		lblNewLabel_1.setBounds(292, 320, 116, 30);
+		welcomePanel.add(lblNewLabel_1);
+		
+		JLabel lblNewLabel_2 = new JLabel("PW :");
+		lblNewLabel_2.setFont(new Font("Arial", Font.BOLD, 30));
+		lblNewLabel_2.setBounds(292, 392, 84, 30);
+		welcomePanel.add(lblNewLabel_2);
+		
+		JButton btnNewButton = new JButton("Log In");
+		btnNewButton.setFont(new Font("Arial", Font.BOLD, 30));
+		btnNewButton.setBounds(374, 471, 243, 50);
+		welcomePanel.add(btnNewButton);
 		frame.setResizable(false);
 		frame.setLocationRelativeTo(null); // 기본위치설정해제
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}
-
 }
 
 class ImagePanel extends JPanel{
