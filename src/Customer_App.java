ec1b7831bda0b25752da75e47a49ce12fb025ae4
ylose <ylose@DESKTOP-PC9EI8M>
2022-02-16 16:23:52 +0900

    로그인버튼 아이콘 추가

--- src/Customer_App.java
+++ src/Customer_App.java
@@ -1,17 +1,17 @@
+import java.awt.BorderLayout;
 import java.awt.Dimension;
 import java.awt.EventQueue;
+import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.Image;
 
 import javax.swing.ImageIcon;
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.JLabel;
-import java.awt.Font;
-import javax.swing.JTextField;
-import javax.swing.SwingConstants;
-import javax.swing.JPasswordField;
 import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JPasswordField;
+import javax.swing.JTextField;
 public class Customer_App {
 	private JFrame frame;
 	private JTextField txtEnterId;
@@ -44,11 +44,11 @@ public class Customer_App {
 		frame = new JFrame();
 		ImagePanel welcomePanel = new ImagePanel(new ImageIcon("C://Users//ylose//eclipse-workspace//Swing Lessons//Image//welcome.jpg").getImage());
 		frame.setSize(welcomePanel.getWidth(), welcomePanel.getHeight());
-		frame.getContentPane().add(welcomePanel);
+		frame.getContentPane().add(welcomePanel, BorderLayout.SOUTH);
 		
 		JLabel lblNewLabel = new JLabel("Log In");
 		lblNewLabel.setFont(new Font("Arial", Font.BOLD, 30));
-		lblNewLabel.setBounds(292, 229, 213, 65);
+		lblNewLabel.setBounds(403, 470, 116, 65);
 		welcomePanel.add(lblNewLabel);
 		
 		txtEnterId = new JTextField();
@@ -72,10 +72,12 @@ public class Customer_App {
 		lblNewLabel_2.setBounds(292, 392, 84, 30);
 		welcomePanel.add(lblNewLabel_2);
 		
-		JButton btnNewButton = new JButton("Log In");
-		btnNewButton.setFont(new Font("Arial", Font.BOLD, 30));
-		btnNewButton.setBounds(374, 471, 243, 50);
-		welcomePanel.add(btnNewButton);
+		JButton btnLogIn = new JButton("");
+		btnLogIn.setIcon(new ImageIcon("C:\\Users\\ylose\\eclipse-workspace\\Swing Lessons\\Image\\login_button.jpg"));
+		btnLogIn.setPressedIcon(new ImageIcon("C:\\Users\\ylose\\eclipse-workspace\\Swing Lessons\\Image\\login_button (2).png"));
+		btnLogIn.setBounds(514, 470, 65, 65);
+		
+		welcomePanel.add(btnLogIn);
 		frame.setResizable(false);
 		frame.setLocationRelativeTo(null); // 기본위치설정해제
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
