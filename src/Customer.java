89231d6abc0d6428739871e0c58d3ad27b80fa4c
ylose <ylose@DESKTOP-PC9EI8M>
2022-02-17 16:12:15 +0900

    테이블 생성 메서드 추가

--- src/Customer.java
+++ src/Customer.java
@@ -1,9 +1,30 @@
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 
 public class Customer {
 	public static void main(String[] args) {
 		getConnection();
+	}
+	
+	public static void createTable() {
+		try {
+			Connection con = getConnection();
+			PreparedStatement createTable = con.prepareStatement(
+					"CREATE TABLE IF NOT EXISTS"
+					+ "customer(id int NOT NULL, AUTO_INCREMENT"
+					+ "name varChar(255),"
+					+ "phone varChar(255),"
+					+ "gender varChar(255),"
+					+ "age varChar(255),"
+					+ "note varChar(255),"
+					+ "PRIMARY KEY(id))");
+			createTable.execute();
+		}catch(Exception e){
+			System.out.println(e.getMessage());
+		} finally {
+			System.out.println("Table successfully created");
+		}
 	}
 	
 	public static Connection getConnection() {
