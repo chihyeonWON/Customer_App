5ac5b7cce751310eed08af2a469b23233601704b
ylose <ylose@DESKTOP-PC9EI8M>
2022-02-17 16:03:46 +0900

    MySql 연결

--- src/Customer.java
+++ src/Customer.java
@@ -1,4 +1,24 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
 
 public class Customer {
+	public static void main(String[] args) {
+		getConnection();
+	}
 	
+	public static Connection getConnection() {
+		try {
+			String driver = "com.mysql.cj.jdbc.Driver";
+			String url = "jdbc:mysql://sql5.freemysqlhosting.net:3306/sql5473430";
+			String user = "sql5473430";
+			String password = "cRmtCjyP4L";
+			Class.forName(driver);
+			Connection con = DriverManager.getConnection(url, user, password);
+			System.out.println("The Connection Successful");
+			return con;
+		}catch(Exception e) {
+			System.out.println(e.getMessage());
+			return null;
+		}
+	}
 }
